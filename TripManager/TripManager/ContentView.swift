b3f9c1e5ed59e4affaba28a809e8744ae3ce9c9d
Daniel Cano <ridedcp@gmail.com>
2025-05-16 17:48:20 +0200

    Create oroject on xcode

---
+++ TripManager/TripManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TripManager
+//
+//  Created by Daniel Cano on 16/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TripManager/TripManager/TripManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripManagerApp.swift
+//  TripManager
+//
+//  Created by Daniel Cano on 16/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TripManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
