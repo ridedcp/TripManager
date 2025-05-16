f3c8c682c10b0b835c2a5c0a0f3a953b115a5bf4
Daniel Cano <ridedcp@gmail.com>
2025-05-16 18:53:54 +0200

    create tripDTO

---
+++ TripManager/Data/DTOs/TripDTO.swift
@@ -0,0 +1,15 @@
+//
+//  TripDTO.swift
+//  TripManager
+//
+//  Created by Daniel Cano on 16/5/25.
+//
+
+import Foundation
+
+struct TripDTO: Decodable {
+    let description: String
+    let driverName: String
+    let startTime: String
+    let endTime: String
+}
