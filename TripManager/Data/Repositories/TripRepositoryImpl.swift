350372cc4686b3ec919e10494ea340754bb02203
Daniel Cano <ridedcp@gmail.com>
2025-05-16 19:57:54 +0200

    create repository implementation

---
+++ TripManager/Data/Repositories/TripRepositoryImpl.swift
@@ -0,0 +1,22 @@
+//
+//  TripRepositoryImpl.swift
+//  TripManager
+//
+//  Created by Daniel Cano on 16/5/25.
+//
+
+import Foundation
+
+final class TripRepositoryImpl: TripRepository {
+    private let service: TripService
+
+    init(service: TripService) {
+        self.service = service
+    }
+
+    func getTrips() async throws -> [Trip] {
+        let dtos = try await service.fetchTrips()
+        return try dtos.map { try TripMapper.map(dto: $0) }
+    }
+}
+
