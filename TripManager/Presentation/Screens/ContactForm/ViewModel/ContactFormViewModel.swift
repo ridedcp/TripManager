9720748540f7649cbdd34fd622267670d75431af
Daniel Cano <ridedcp@gmail.com>
2025-05-21 10:21:10 +0200

    Create contactform view and viewmodel

---
+++ TripManager/Presentation/Screens/ContactForm/ViewModel/ContactFormViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ContactFormViewModel.swift
+//  TripManager
+//
+//  Created by Daniel Cano on 21/5/25.
+//
+
+import Foundation
---
+++ TripManager/Presentation/Screens/ContactForm/Views/ContactFormView.swift
@@ -0,0 +1,8 @@
+//
+//  ContactFormView.swift
+//  TripManager
+//
+//  Created by Daniel Cano on 21/5/25.
+//
+
+import Foundation
